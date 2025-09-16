4f97bbf64eeac1c672dd8399f242c58c4afc1e0c
scabezas <scabezas@ciisa.cl>
2025-09-16 20:44:48 +0000

    test

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -345,7 +345,7 @@
         @endauth
     </div>
 
-    <img src="{{ asset('assets/imgs/Jugadores_Sonkei.webp') }}" class="landing-image" alt="Dos jugadores en el campo">
+    <img src="{{ asset('imgs/Jugadores_Sonkei.webp') }}" class="landing-image" alt="Dos jugadores en el campo">
     <hr class="my-5 mb-5">
 
     {{-- Carrusel --}}
